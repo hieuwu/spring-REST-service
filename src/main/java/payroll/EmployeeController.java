5d8844a74c114f259939bfd4587415c5c99bd1fe
hieuvu99 <star.hieu.99@gmail.com>
2020-10-05 15:45:44 +0700

    refactor: controller

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -67,10 +67,12 @@ public class EmployeeController {
                 .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
                 .body(entityModel);
     }
-
     @DeleteMapping("/employees/{id}")
-    void deleteEmployee(@PathVariable Long id) {
+    ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
+
         repository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 
 }
